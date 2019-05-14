dabdf23b5ee2516d83ee807d519e7311971fd0fa
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-14 15:18:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,16 +1,31 @@
-people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-
-
 def badge_maker(name)
-return "Hello, my name is #{name}."
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  badges=[]
+  attendees.each do|badge|
+  badges.push("Hello, my name is #{badge}.")
+  end
+  return badges
+  
+end 
+
+def assign_rooms(speakers)
+  assign=[]
+  
+  speakers.each_with_index{|speaker,index|
+  assign.push("Hello, #{speaker}! You'll be assigned to room #{index + 1}!")
+    }
+   return assign
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
   end
   
- def batch_badge_creator(name)
-   array = []
-    array.each do |name|
-     newarray.push("Hello, my name is #{name}.")
-   end 
-   return new array 
- end
-   
-  
+  assign_rooms(attendees).each do |badge|
+    puts badge
+  end
+end 
