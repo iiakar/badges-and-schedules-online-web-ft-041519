1daa79269ced5193fad26eede1175a021982ff0c
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-14 15:15:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,16 +10,10 @@ end
   
  def batch_badge_creator(name)
    newarray = []
-   
-   array.each do |name|
+    array.each do |name|
      newarray.push ("Hello, my name is #{name}.")
    end 
+   return new array 
    
-   it 'should return a list of badge messages' do
-      expect(batch_badge_creator(attendees)).to eq(badges)
-    end
-    it 'should not hard-code response' do
-      expect(batch_badge_creator(["Johnny"])).to eq(["Hello, my name is Johnny."])
-    end
-
-  end
+   
+  
