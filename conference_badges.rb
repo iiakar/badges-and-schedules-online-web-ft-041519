8b6c95cd2c540db2caceaefb09fceb62dde42890
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-14 15:16:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,7 +8,7 @@ def badge_maker(name)
   
 end
   
- def batch_badge_creator(name)
+ def batch_badge_creator(array)
    array = []
     array.each do |name|
      newarray.push ("Hello, my name is #{name}.")
