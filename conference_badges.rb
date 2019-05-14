ef2d3d39956f827823c2fbf8d7e6cd352bb5e29b
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-14 15:13:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,3 +9,14 @@ def badge_maker(name)
 end
   
  def batch_badge_creator
+   
+   
+   
+   it 'should return a list of badge messages' do
+      expect(batch_badge_creator(attendees)).to eq(badges)
+    end
+    it 'should not hard-code response' do
+      expect(batch_badge_creator(["Johnny"])).to eq(["Hello, my name is Johnny."])
+    end
+
+  end
